4e97a4922c683ed00435a09e4f60c065bff01641
Pasqualito Elbuhel <pelbuhel57@gmail.com>
2025-02-23 18:28:22 -0800

    Renamed untitled folder and remove string to test commit

--- CalcEngine/src/Main.java
+++ CalcEngine/src/Main.java
@@ -2,14 +2,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String poo = "poo";
-
 
         double[] firstValue = {100.0d, 25.0d, 225.0d, 11.0d};
         double[] secondValue = {50.0d, 92.0d, 17.0d, 3.0d};
         char[] opCodes = {'d', 'a', 's', 'm'};
+        //Place value in the array brackets to make space for values since we do not want to initialize with values.
         double[] results = new double[opCodes.length];
-
 
         for (int index = 0; index < opCodes.length; index++){
         switch(opCodes[index]) {
@@ -33,6 +31,10 @@ public class Main {
                 break;
             }
         }
+
+        //Below, we are declaring the double variable currentResult. For each value in the array results, we are going to print the
+        for(double currentResult : results)
+            System.out.println(currentResult);
         System.out.println(Arrays.toString(results));
 
 //        if (opCode == 'a')
@@ -41,9 +43,9 @@ public class Main {
 //            result = value1 - value2;
 //        else if (opCode == 'm')
 //            result = value1 * value2;
-////        else if (value2 == 0 && opCode == 'd') {
-////            System.out.println("0 cannot be a divisor");
-////        }
+//        else if (value2 == 0 && opCode == 'd') {
+//            System.out.println("0 cannot be a divisor");
+//        }
 //        // With no {} around below statement, else statement is tied to if (value2 !=0)
 //        else if (opCode == 'd') {
 //            if (value2 != 0)
